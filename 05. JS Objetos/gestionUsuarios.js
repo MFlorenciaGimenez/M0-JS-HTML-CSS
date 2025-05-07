0e844fa92ba29f9199b07b3c928306a11ec5544c
MFlorenciaGimenez <florgimenez14@hotmail.com>
2025-05-07 20:29:02 -0300

    terminado objetos, terminado el trabajo

--- 05. JS Objetos/gestionUsuarios.js
+++ 05. JS Objetos/gestionUsuarios.js
@@ -3,22 +3,44 @@ const { perfiles } = require("../build/js/perfiles.js")
 
 var asistente = {
     verPerfiles: function(opcion){
-        /* TU CODIGO */
-      
+        if(opcion==="todo") return perfiles
+        if(opcion==="nombre") return perfiles.map((perfiles)=>perfiles.usuario)
+        if(opcion==="codigo") return perfiles.map((perfiles)=>perfiles.codigo)
+        if(opcion==="nivel") return perfiles.map((perfiles)=>perfiles.nivel_de_autorizacion)
+        if(opcion==="antiguedad") return perfiles.map((perfiles)=>perfiles.antiguedad)
     },
     
-    verPerfilesPorAntiguedad: function(){
-        /* TU CODIGO */
+   verPerfilesPorAntiguedad: function(){
+
+        let NuevoArray=[]
+
+        for(let i=0; i<perfiles.length; i++){
+            NuevoArray.push(perfiles[i])
+        }
+
+        return NuevoArray.sort(function(primero, segundo ){
+            return segundo.antiguedad - primero.antiguedad;
+        })
+
+        
         
     },
 
     verAdministradores: function(){
-        /* TU CODIGO */
-
+        return  perfiles.filter((admin)=> admin.nivel_de_autorizacion==="admin" )
+        
     },
 
     modificarAcceso: function(usuario, codigo){
-        /* TU CODIGO */
+        let encontrado= perfiles.find((perfil)=> perfil.usuario===usuario ) 
+        if (!encontrado) return "usuario no encontrado"
+
+        if(codigo.length !==4 || isNaN(codigo)) return "codigo de acceso invalido, debe contener solo 4 numeros"          
+        
+        encontrado.codigo = codigo 
+        return "codigo de acceso modificado"
+
+        
         
     }
 }
@@ -27,4 +49,3 @@ var asistente = {
 module.exports = {
     asistente
 }
-
